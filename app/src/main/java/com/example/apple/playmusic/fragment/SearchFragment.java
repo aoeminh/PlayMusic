6897418593006b3e2b0d49377a809e7398abfb6d
Minh <aoeminh@gmail.com>
2019-04-10 16:29:17 +0700

    save function hide key board

--- app/src/main/java/com/example/apple/playmusic/fragment/SearchFragment.java
+++ app/src/main/java/com/example/apple/playmusic/fragment/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.apple.playmusic.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -103,4 +104,36 @@ public class SearchFragment extends Fragment implements View.OnTouchListener, IS
         intent.putParcelableArrayListExtra("song",lovesongs);
         startActivity(intent);
     }
+
+
+    I found a better solution:
+
+    Override the dispatchTouchEvent method in your Activity .
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+
+        if (v != null &&
+                (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + v.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + v.getTop() - scrcoords[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
+
 }
